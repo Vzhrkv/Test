a24782c5bdc1f0b1e6ef3b7d20ae9ff748758f4d
Vzrhkv <lieden57@gmail.com>
2024-02-27 20:57:53 +0300

    add functions for pitch shift and time stretch

---
+++ app/modification/__init__.py
@@ -0,0 +1,7 @@
+from app.modification.modification import shift_pitch, stretch_audio
+
+
+__all__ = [
+    "shift_pitch",
+    "stretch_audio",
+]
---
+++ app/modification/modification.py
@@ -0,0 +1,40 @@
+from typing import Tuple
+from librosa.effects import pitch_shift, time_stretch
+from numpy import ndarray
+
+from app.base import ReadedAudio
+
+
+ProcessedAudio = Tuple[ndarray, float]
+
+
+def shift_pitch(
+    readed_audio: ReadedAudio,
+    steps: int,
+    bins_per_octave: int,
+) -> ProcessedAudio:
+    if bins_per_octave <= 0:
+        raise ValueError(
+            f"Bins per octave must be greater than zero(Given bins: {bins_per_octave})"
+        )
+    y, sr = readed_audio
+    if steps == 0:
+        return y, sr
+
+    shifted_y = pitch_shift(
+        y=y,
+        sr=sr,
+        n_steps=steps,
+        bins_per_octave=bins_per_octave,
+    )
+    return shifted_y, sr
+
+
+def stretch_audio(
+    readed_audio: ReadedAudio,
+    rate: float,
+) -> ProcessedAudio:
+    if rate <= 0:
+        raise ValueError(f"Time stretch can't be less than zero(Given rate: {rate}).")
+
+    return time_stretch(y=readed_audio[0], rate=rate), readed_audio[1]
